43fe9b447b1854ef550c52108cbd2343168c1b69
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-06-06 16:34:31 +0200

    Execute first fuzzer

--- fuzz/fuzz.ml
+++ fuzz/fuzz.ml
@@ -74,7 +74,7 @@ let uniq =
   let v = ref (-1) in
   fun () -> incr v ; !v
 
-let lol () =
+let () =
   Crowbar.add_test ~name:"z/zlib" [ Crowbar.bytes ] @@ fun bytes ->
   Fmt.epr "Test %S.\n%!" bytes ;
   let res0 = zlib bytes in
